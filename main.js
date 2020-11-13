2cc697c94087a97178316137d229a512d28b0a63
Patryk <ptomaszewskig3@gmail.com>
2020-11-13 01:04:15 +0100

    ekrany

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
+
+// $(document).ready(function () {
+//     $('#dtBasicExample').DataTable();
+//     $('.dataTables_length').addClass('bs-select');
+// });
