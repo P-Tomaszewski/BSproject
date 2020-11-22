c3534fd36e04665ff1aa6a80c196bdaccabb5df2
Patryk <ptomaszewskig3@gmail.com>
2020-11-22 19:55:16 +0100

    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>

--- js/validateAddDriver.js
+++ js/validateAddDriver.js
@@ -11,35 +11,35 @@ var elAddDriver = document.getElementById('addDriver');
 
 function checkName(minLength){
     if(elName.value.length < minLength){
-        elMsgName.textContent = 'Imie zawodnika musi mieć conajmniej ' + minLength + ' znaków!';
+        elMsgName.textContent = 'Imie zawodnika nie może być puste!';
     } else {
         elMsgName.innerHTML='';
     }
 }
 function checkLastname(minLength){
     if(elLastname.value.length < minLength){
-        elMsgLastname.textContent = 'Nazwisko zawodnika musi mieć conajmniej ' + minLength + ' znaków!';
+        elMsgLastname.textContent = 'Nazwisko zawodnika nie może być puste';
     } else {
         elMsgLastname.innerHTML='';
     }
 }
 function checkTeam(minLength){
     if(elTeam.value.length < minLength){
-        elMsgTeam.textContent = 'Team zawodnika musi mieć conajmniej ' + minLength + ' znaków!';
+        elMsgTeam.textContent = 'Team zawodnika nie może być pusty';
     } else {
         elMsgTeam.innerHTML='';
     }
 }
 function checkCar(minLength) {
     if (elCar.value.length < minLength) {
-        elMsgCar.textContent = 'Nazwa samochodu zawodnika musi mieć conajmniej ' + minLength + ' znaków!';
+        elMsgCar.textContent = 'Nazwa samochodu zawodnika nie może być pusta';
     } else {
         elMsgCar.innerHTML = '';
     }
 }
 
     elAddDriver.addEventListener('click', function () {
-        checkName(5);
+        checkName(1);
         checkLastname(1);
         checkTeam(1);
         checkCar(1);
